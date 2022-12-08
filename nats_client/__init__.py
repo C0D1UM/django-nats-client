445ea04fd3b6022f6b06e813328401778cc621d9
Preeti Yuankrathok <preetisatit@gmail.com>
2022-12-08 15:20:20 +0700

    Add send method

--- nats_client/__init__.py
+++ nats_client/__init__.py
@@ -1,3 +1,3 @@
 # pylint: disable=unused-import
-from .clients import request, request_async
+from .clients import request, request_async, send, send_async
 from .registry import register
--- nats_client/clients.py
+++ nats_client/clients.py
@@ -23,5 +23,21 @@ async def request_async(subject_name: str, method_name: str, *args, **kwargs) ->
     return json.loads(data)['result']
 
 
+async def send_async(subject_name: str, method_name: str, *args, **kwargs) -> None:
+    payload = parse_arguments(method_name, args, kwargs)
+
+    nc = Client()
+    await nc.connect(**settings.NATS_OPTIONS)
+
+    try:
+        await nc.publish(subject_name, payload)
+    finally:
+        await nc.close()
+
+
 def request(*args, **kwargs):
     return asyncio.run(request_async(*args, **kwargs))
+
+
+def send(*args, **kwargs):
+    return asyncio.run(send_async(*args, **kwargs))
