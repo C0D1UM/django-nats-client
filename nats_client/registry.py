93ed10ab44b20afd23eb5a9332ee1409285a0f44
Preeti Yuankrathok <preetisatit@gmail.com>
2023-06-10 01:10:33 +0700

    Update registry to support JS

--- nats_client/registry.py
+++ nats_client/registry.py
@@ -1,35 +1,40 @@
+from django.conf import settings
+
+
 class FunctionRegistry:
     """Function registry for callback functions from NATS"""
 
     def __init__(self):
         self.registry = {}
 
-    def register(self, name=None, func=None):
-        # @register()
-        if name is None and func is None:
-            return self.register_function
-
+    def register(self, name=None, func=None, **kwargs):
         # register(...)
         if func is not None:
-            return self.register_function(func, name)
+            return self.register_function(func, name, **kwargs)
 
         # @register
         if name is not None and func is None and callable(name):
-            return self.register_function(name)
+            return self.register_function(name, **kwargs)
 
         # @register(...)
         def dec(func):
-            return self.register(name, func)
+            return self.register(name, func, **kwargs)
 
         return dec
 
-    def register_function(self, func, name: str = None):
-        name = name or getattr(func, '_decorated_function', func).__name__
+    def register_function(self, func, name: str = None, **kwargs):
+        subject = kwargs.get('subject', getattr(settings, 'NATS_LISTENING_SUBJECT', 'default'))
+        func_name = getattr(func, '_decorated_function', func).__name__
+        name = f'{subject}.{name or func_name}'
 
         if name in self.registry:
             raise ValueError(f'Duplicated NATS function named `{name}`.')
 
-        self.registry[name] = func
+        self.registry[name] = {
+            'func': func,
+            'subject': subject,
+            'js': kwargs.get('js', False),
+        }
         return func
 
 
